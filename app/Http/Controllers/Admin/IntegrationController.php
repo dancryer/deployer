ea9c2bc94ca7ad6bcab3b888001df7c359147cd3
Stephen Ball <stephen@rebelinblue.com>
2015-06-26 21:41:38 +0100

    Playing around a bit more with oauth

--- app/Http/Controllers/Admin/IntegrationController.php
+++ app/Http/Controllers/Admin/IntegrationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use League\OAuth1\Client\Server\Bitbucket;
+use League\OAuth2\Client\Provider\Github;
 use Session;
 
 use GuzzleHttp\Client;
@@ -20,17 +21,32 @@ class IntegrationController extends Controller
 
     public function auth($service)
     {
+        if ($service === 'bitbucket')
+        {
+            $server = $this->bitbucket();
+            
+            // Retrieve temporary credentials
+            $temporaryCredentials = $server->getTemporaryCredentials();
 
-        $server = $this->bitbucket();
-        
-        // Retrieve temporary credentials
-        $temporaryCredentials = $server->getTemporaryCredentials();
+            Session::flash('temporary_credentials', serialize($temporaryCredentials));
 
-        Session::flash('temporary_credentials', serialize($temporaryCredentials));
+            // Second part of OAuth 1.0 authentication is to redirect the
+            // resource owner to the login screen on the server.
+            $url = $server->getAuthorizationUrl($com);
+        }
+        else if ($service === 'github')
+        {
+            $provider = $this->github();
 
-        // Second part of OAuth 1.0 authentication is to redirect the
-        // resource owner to the login screen on the server.
-        $url = $server->getAuthorizationUrl($temporaryCredentials);
+            // If we don't have an authorization code then get one
+            $url = $provider->getAuthorizationUrl();
+
+
+            Session::flash('oauth2state', $provider->state);
+
+        }
+
+
 
         return redirect($url);
     }
@@ -48,27 +64,57 @@ class IntegrationController extends Controller
 
             // We will now retrieve token credentials from the server
             $tokenCredentials = $server->getTokenCredentials($temporaryCredentials, $_GET['oauth_token'], $_GET['oauth_verifier']);
-
-
-
-$client = new Client(['base_url' => 'https://api.bitbucket.org/2.0/']);
-
-$oauth = new Oauth1([
-    'consumer_key'    => 'gS2JYF5M69zxgRu8G3',
-    'consumer_secret' => 'BeQaWZPyYPwBQyRMHTSNsUFGrPPxaAZR',
-    'token'           => $tokenCredentials->getIdentifier(),
-    'token_secret'    => $tokenCredentials->getSecret()
-]);
-
-$client->getEmitter()->attach($oauth);
-
-// Set the "auth" request option to "oauth" to sign using oauth
-$res = $client->get('repositories/rebelinblue', ['auth' => 'oauth']);
-
-dd($res->json());
-
-
         }
+
+        if ($service === 'bitbucket') {
+            $client = new Client(['base_url' => 'https://api.bitbucket.org/2.0/']);
+
+            $oauth = new Oauth1([
+                'consumer_key'    => 'gS2JYF5M69zxgRu8G3',
+                'consumer_secret' => 'BeQaWZPyYPwBQyRMHTSNsUFGrPPxaAZR',
+                'token'           => $tokenCredentials->getIdentifier(),
+                'token_secret'    => $tokenCredentials->getSecret()
+            ]);
+
+            $client->getEmitter()->attach($oauth);
+
+            // Set the "auth" request option to "oauth" to sign using oauth
+            $res = $client->get('repositories/rebelinblue', ['auth' => 'oauth']);
+        }
+        else if ($service === 'github')
+        {
+            if (empty($_GET['state']) || ($_GET['state'] !== Session::get('oauth2state'))) {
+
+                exit('Invalid state');
+
+            }
+
+            $provider = $this->github();
+
+            $token = $provider->getAccessToken('authorization_code', [
+                'code' => $_GET['code']
+            ]);
+
+            // Optional: Now you have a token you can look up a users profile data
+            try {
+
+                // We got an access token, let's now get the user's details
+                $userDetails = $provider->getUserDetails($token);
+
+                // Use these details to create a new profile
+                printf('Hello %s!', $userDetails->firstName);
+
+            } catch (Exception $e) {
+
+                // Failed to get user details
+                exit('Oh dear...');
+            }
+
+            dd($token);
+            exit;
+        }
+
+        dd($res->json());
     }
 
     private function bitbucket()
@@ -78,7 +124,20 @@ dd($res->json());
             'secret' => 'BeQaWZPyYPwBQyRMHTSNsUFGrPPxaAZR',
             'callback_uri' => url('admin/integrations/callback', ['service' => 'bitbucket']),
         ]);
+
+
         
+        return $server;
+    }
+
+    private function github()
+    {
+        $server = new Github([
+            'clientId'      => '4bbf037827ace2e23b1f',
+            'clientSecret'  => 'beace660c4c6d4c0ecefac133c7997364d07228c',
+            'redirectUri'   => url('admin/integrations/callback', ['service' => 'github']),
+            'scopes'        => ['user', 'repo'],
+        ]);
 
         return $server;
     }
